4f9525ebbd70e589e9961fc899b826e2d1d8f41d
margo <brownmar24@gmail.com>
2022-02-08 14:30:03 -0800

    Initial commit

---
+++ app/src/main/java/com/example/hw1/houseView.java
@@ -0,0 +1,11 @@
+package com.example.hw1;
+
+import android.content.Context;
+import android.view.SurfaceView;
+
+public class houseView extends SurfaceView {
+
+    public houseView(Context context) {
+        super(context);
+    }
+}
